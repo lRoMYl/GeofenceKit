f5e0c67486a11f582c16bf434faa302b5d10c89b
Romy Cheah <romy.cheah@thecarousell.com>
2020-01-18 18:26:16 +0800

    Added some test case for Demo app

--- Demo/DemoTests/DemoTests.swift
+++ Demo/DemoTests/DemoTests.swift
@@ -3,6 +3,7 @@
 
 import XCTest
 @testable import Demo
+import GeofenceKit
 
 class DemoTests: XCTestCase {
 
@@ -14,16 +15,79 @@ class DemoTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testInitialState() {
+        let expectation = XCTestExpectation(description: "Wait for geofence result")
+        
+        struct MockUserLocationProvider: UserLocationProvider {
+            var delegate: UserLocationProviderDelegate?
+            
+            var location: UserLocation? = UserLocation(
+                latitude: 3.11, longitude: 103.10, wifiSsid: "test-wifi")
+            
+            func startMonitoring() { }
+            
+            func stopMonitoring() { }
         }
+        
+        let homeView = HomeView(
+            viewModel: HomeViewModel(
+                policy: DefaultPolicy(),
+                userLocationProvider: MockUserLocationProvider(),
+                overrideUserLocationProvider: MockUserLocationProvider()))
+        
+        homeView.viewModel.onTapMonitor()
+        
+        DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + 3) {
+            XCTAssertTrue(
+                homeView.viewModel.title == "IN REGION",
+                """
+                viewModel.overrideUserLocation is not true yet, so user location
+                should still be (0, 0) making the result to be IN REGION
+                """)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
     }
-
+    
+    func testOverrideUserLocationState() {
+        let expectation = XCTestExpectation(description: "Wait for geofence result")
+        
+        struct MockUserLocationProvider: UserLocationProvider {
+            var delegate: UserLocationProviderDelegate?
+            
+            var location: UserLocation? = UserLocation(
+                latitude: 3.11, longitude: 103.10, wifiSsid: "test-wifi")
+            
+            func startMonitoring() {
+                delegate?.userLocationProvider(self, didReceive: location!)
+            }
+            
+            func stopMonitoring() { }
+        }
+        
+        let homeView = HomeView(
+            viewModel: HomeViewModel(
+                policy: DefaultPolicy(),
+                userLocationProvider: MockUserLocationProvider(),
+                overrideUserLocationProvider: MockUserLocationProvider()))
+        
+        homeView.viewModel.userLocationOverride = false
+        homeView.viewModel.onTapMonitor()
+        
+        DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + 3) {
+            XCTAssertTrue(
+                homeView.viewModel.title == "OUTSIDE REGION",
+                """
+                viewModel.overrideUserLocation is false, so user location
+                should still be (3.11, 103.10) making the result to be
+                OUTSIDE REGION
+                """)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
 }
