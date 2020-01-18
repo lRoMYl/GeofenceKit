67982da3acc2bf9d38f9ab2cbff23b618db0651b
Romy Cheah <romy.cheah@thecarousell.com>
2020-01-18 12:48:44 +0800

    Initialize base workspace and projects

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,16 @@
+// Copyright © 2020 Carousell. All rights reserved.
+// 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeofenceKit/GeofenceKit/GeofenceKit.h
@@ -0,0 +1,14 @@
+// Copyright © 2020 Carousell. All rights reserved.
+// 
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GeofenceKit.
+FOUNDATION_EXPORT double GeofenceKitVersionNumber;
+
+//! Project version string for GeofenceKit.
+FOUNDATION_EXPORT const unsigned char GeofenceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeofenceKit/PublicHeader.h>
+
+
